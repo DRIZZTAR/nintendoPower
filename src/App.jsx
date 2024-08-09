fce1899d76fd4fddda93891cd965703bb2c75dd5
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-08-08 21:38:00 -0600

    post processing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Loader } from '@react-three/drei';
+import { AsciiRenderer, Loader } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
 import { Experience } from './components/Experience';
@@ -10,7 +10,7 @@ function App() {
 		<>
 			<UI />
 			<Loader />
-			<Canvas shadows camera={{ position: [-0.5, 2, 4], fov: 35,  }}>
+			<Canvas shadows camera={{ position: [-0.5, 2, 4], fov: 35 }}>
 				<group position-y={0.1}>
 					<Suspense fallback={null}>
 						<Experience />
